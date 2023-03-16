fb7eaa111c02911313b02b6f55096de0d80a8d80
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-03-16 05:58:18 +0100

    WIP still fixing offline cache

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,7 +1,6 @@
+//@ts-nocheck
 export const db = new Promise<Dexie>((resolve, reject) => {
-    //@ts-ignore
     if (window?.Dexie) {
-        //@ts-ignore => this if block litteraly checks if window.Dexie exists
         const db = new window.Dexie("helloworldtodos", {})
         db.version(2).stores({
             tags: "&key",
