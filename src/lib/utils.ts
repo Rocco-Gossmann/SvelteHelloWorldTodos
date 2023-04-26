2af78dca187fd39d22ae6a4faa13f53df6f0b6fb
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-26 06:48:05 +0200

    Added function Utils::isArrayEqual - compares to 1 arrays and their content

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,26 @@ export function isObject(obj: unknown): obj is Object {
 export function isArray(arr: unknown): arr is [] { 
     return arr instanceof Array
 }
+
+export function isArrayEqual(arr1: Array<any>, arr2: Array<any>) {
+    if(
+        (arr1 instanceof Array && !(arr2 instanceof Array)) 
+        || 
+        (arr2 instanceof Array && !(arr1 instanceof Array)) 
+    ) return false;
+
+    if(!(arr1 instanceof Array && arr2 instanceof Array))
+        throw new Error("both values given are not arrays");
+
+    if(arr2 instanceof Array && !(arr1 instanceof Array)) return false
+    if(arr1.length != arr2.length) return false;
+
+    if(arr1.filter( (e, i) => { 
+        if(e instanceof Array && arr2[i] instanceof Array)
+            return !isArrayEqual(e, arr2[i]);
+        else 
+            return !(e === arr2[i]); 
+    }).length) return false;
+
+    return true;
+}
